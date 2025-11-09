a78977403342d8f8f876967812b9a0e189495f56
Simon Samuel <salviensky@gmail.com>
2025-11-09 18:11:37 +0100

    feat: added ticker route to http server feat: added logging middleware

--- api/handlers/ticker_handler.go
+++ api/handlers/ticker_handler.go
@@ -1,1 +1,27 @@
 package handlers
+
+import (
+	"net/http"
+)
+
+func GetTicker(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(id))
+}
+
+func CreateTicker(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func GetTickers(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func DeleteTicker(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(id))
+}
