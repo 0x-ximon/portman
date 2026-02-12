bee9066171a5597b8eb1e0c45f2b34621fa8f236
Simon Samuel <salviensky@gmail.com>
2026-02-12 18:44:18 +0100

    feat(bots): bots can now retrieve last price from api

--- bots/common/models.py
+++ bots/common/models.py
@@ -25,13 +25,37 @@ class User(BaseModel):
     free_balance: Decimal = Field(alias="free_balance")
     frozen_balance: Decimal = Field(alias="frozen_balance")
 
-    role: Role
     password: str
+    role: Role = Field(default=Role.REGULAR)
     api_key: Optional[str] = Field(default=None, alias="api_key")
 
     created_at: datetime = Field(alias="created_at")
     updated_at: datetime = Field(alias="updated_at")
     deleted_at: Optional[datetime] = Field(default=None, alias="deleted_at")
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        from_attributes=True,
+    )
+
+
+class Status(str, Enum):
+    OPEN = "OPEN"
+    CLOSED = "CLOSED"
+    SUSPENDED = "SUSPENDED"
+
+
+class Ticker(BaseModel):
+    id: int
+
+    base: str = Field(min_length=3, max_length=10)
+    quote: str = Field(min_length=3, max_length=10)
+    symbol: str = Field(min_length=3, max_length=10)
+
+    ask: Decimal = Field(default=Decimal("0.0"), ge=Decimal("0.0"))
+    bid: Decimal = Field(default=Decimal("0.0"), ge=Decimal("0.0"))
+    last: Decimal = Field(default=Decimal("0.0"), ge=Decimal("0.0"))
+    status: Status = Field(default=Status.CLOSED)
 
     model_config = ConfigDict(
         populate_by_name=True,
--- bots/core/manager.py
+++ bots/core/manager.py
@@ -1,14 +1,37 @@
+import random
 import asyncio
 import httpx
 
 from core.worker import Worker
+from common.models import Ticker, Status
 
 
 class Manager:
+    tickers: list[Ticker] = []
+
     async def start(self, base_url: str, bots_amount: int) -> None:
         async with httpx.AsyncClient(
             base_url=base_url,
             headers={"Content-Type": "application/json"},
         ) as shared_client:
-            bots = [Worker(i, shared_client) for i in range(1, bots_amount + 1)]
+            await self.get_tickers(shared_client)
+
+            bots = [
+                Worker(i, shared_client, self.random_ticker())
+                for i in range(1, bots_amount + 1)
+            ]
             await asyncio.gather(*(bot.run() for bot in bots))
+
+    # TODO: Implement error handling
+    async def get_tickers(self, client: httpx.AsyncClient) -> None:
+        response = await client.get("/tickers")
+        response.raise_for_status()
+
+        data = response.json()["data"]
+        for t in data:
+            ticker = Ticker.model_validate(t)
+            if ticker.status == Status.OPEN:
+                self.tickers.append(ticker)
+
+    def random_ticker(self) -> str:
+        return self.tickers[random.randint(0, len(self.tickers) - 1)].symbol
