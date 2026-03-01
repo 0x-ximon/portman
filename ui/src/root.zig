7ee75713801ad623467da6d0e0ef3cc6b5dddbac
Simon Samuel <salviensky@gmail.com>
2026-03-01 22:35:41 +0100

    chore(ui): linting changes

--- ui/src/root.zig
+++ ui/src/root.zig
@@ -4,5 +4,5 @@ const vaxis = @import("vaxis");
 const vxfw = vaxis.vxfw;
 
 pub const Chart = @import("widgets/chart.zig");
-pub const Router = @import("widgets/router.zig");
 pub const Navigator = @import("widgets/navigator.zig");
+pub const Router = @import("widgets/router.zig");
