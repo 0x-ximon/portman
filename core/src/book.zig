f1494a9b5a464035e7a513142d472d3dd5afdd18
Simon Samuel <salviensky@gmail.com>
2026-07-04 15:45:51 +0100

    chore: added all or none placeholder order mode

--- core/src/book.zig
+++ core/src/book.zig
@@ -30,6 +30,7 @@ const Mode = enum(u8) {
     gtc = 0,
     fok = 1,
     ioc = 2,
+    aon = 3,
 };
 
 const Flags = packed struct(u8) {
@@ -100,6 +101,7 @@ pub fn newOrder(self: *Book, order: *const Order) !void {
         .gtc => try gtc(self, order),
         .fok => try fok(self, order),
         .ioc => try ioc(self, order),
+        .aon => try aon(self, order),
     }
 }
 
@@ -134,6 +136,14 @@ fn fok(_: *Book, order: *const Order) !void {
 
 // Immediate or Cancel
 fn ioc(_: *Book, order: *const Order) !void {
+    switch (order.side) {
+        .buy => {},
+        .sell => {},
+    }
+}
+
+// All or None
+fn aon(_: *Book, order: *const Order) !void {
     switch (order.side) {
         .buy => {},
         .sell => {},
