50efb9062d11f87366afd8e9525e633d7e9ba86c
Simon Samuel <salviensky@gmail.com>
2026-07-28 21:51:46 +0100

    feat(core): implemented messaging queue using nats.zig

--- core/src/packet.zig
+++ core/src/packet.zig
@@ -22,19 +22,9 @@ pub const Header = extern struct {
     destination: u64,
 };
 
-pub fn info(reader: *Io.Reader) !*const Header {
-    const bytes = try reader.take(@sizeOf(Header));
-    const header: *const Header = @ptrCast(@alignCast(bytes.ptr));
-    return header;
-}
-
-pub fn recv(reader: *Io.Reader, length: usize) ![]Order {
-    const bytes = try reader.take(length);
-    const count = length / @sizeOf(Order);
-    const orders = @as([*]Order, @ptrCast(@alignCast(bytes.ptr)))[0..count];
-    return orders;
-}
-
-pub fn send(writer: Io.Writer) void {
-    _ = writer;
+pub fn recv(bytes: []const u8) struct { *const Header, []Order } {
+    const size = @sizeOf(Header);
+    const header: *const Header = @ptrCast(@alignCast(bytes[0..size]));
+    const orders: []Order = @ptrCast(@alignCast(@constCast(bytes[size..])));
+    return .{ header, orders };
 }
